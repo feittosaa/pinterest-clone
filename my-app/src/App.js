b3b3e0be9c5675a271faecb2e810065a81b838ea
feittosaa <lucas.feitosa2004@gmail.com>
2022-10-21 18:55:45 -0300

    init

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="hello-world">
+        <p>hello world</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
